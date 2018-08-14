eed76663889fb8f11b5f402614ddff8ec340b1ca
denmas <34563717+denmasgeo@users.noreply.github.com>
2018-08-14 23:36:45 +0700

    polles
    add

--- linepy/config.py
+++ linepy/config.py
@@ -30,7 +30,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'NADYA_TJ'
+    SYSTEM_NAME = 'USA_geo'
     SYSTEM_VER  = '12.1.1'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
